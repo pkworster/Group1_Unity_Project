544503d26283fdda9904825cde271f775c74eef1
pkworster <Pk101784!>
2021-11-22 14:39:57 -0500

    V1.671
    cleared player health script

--- Group1_Unity_Project/Assets/Scripts/Player_Health.cs
+++ Group1_Unity_Project/Assets/Scripts/Player_Health.cs
@@ -4,45 +4,10 @@ using UnityEngine;
 
 public class Player_Health : MonoBehaviour
 {
-
-    public int maxHealth = 100;
-    public int currentHealth;
-
-    public Health_Bar health_Bar;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        currentHealth = maxHealth;
-        health_Bar.maxValue = maxHealth;
-    }
-
-    // Update is called once per frame
-    public void UpdateHealth(float mod)
-    {
-        if (currentHealth > maxHealth)
-        {
-            currentHealth = maxHealth;
-        }
-        else if (currentHealth <= 0f)
-        {
-            currentHealth = 0f;
-            health_Bar.value = currentHealth;
-            PlayerDied();
-        }
-    }
-    private void ()
-        {
-        LevelManager.instance.GameOver();
-        }
-        
-    void TakeDamage(int damage)
-    {
-        currentHealth -= damage;
-
-        health_Bar.SetHealth(currentHealth);
-    }
 }
 
+
+
 // player health script - Peter Worster
-// Test Parts out - death screen tie ins - Peter Worster 11/22
+// Test Parts out - death screen tie ins - Peter Worster 11/21
+// wiped out... going to try something different 11/22
