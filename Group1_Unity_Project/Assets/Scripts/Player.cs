ee9849c232e0d3f02a83cd663f7d1dee8bb1e8c5
pkworster <Pk101784!>
2021-11-29 11:00:37 -0500

    V1.76
    Player Health/ Fall damage

---
+++ Group1_Unity_Project/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // attempting class within a class... in the event we want to do more things than a health bar
+
+    [System.Serializable]
+    public class PlayerStats
+    {
+        public float Health = 100;
+    }
+    public PlayerStats playerStats = new PlayerStats();
+
+    public int fallBoundary = -40;
+
+    private void Update()
+    {
+        if (transform.position.y <= fallBoundary)
+            DamagePlayer(999999999);
+    }
+
+
+    public void DamagePlayer (int damage)
+    {
+        playerStats.Health -= damage;
+        if (playerStats.Health <=0)
+        {
+            GameMaster.KillPlayer(this);
+        }
+    }
+    private void Start()
+    {
+        
+    }
+
+}
+
+
+
+// player health script - Peter Worster
+// Test Parts out - death screen tie ins - Peter Worster 11/21
+// wiped out... going to try something different 11/22
+// 2nd or 3rd attempt 11/28
